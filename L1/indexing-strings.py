eccde19385d09a2ce308f426c3aced7e8f574abe
Marina M <mansumdp@gmail.com>
2017-06-25 00:38:19 -0300

    Add files via upload

---
+++ L1/indexing-strings.py
@@ -0,0 +1,4 @@
+name = 'Dave'
+print 'Emily'[3]
+print name[-1]
+print name [1]
---
+++ L1/sub-sequences.py
@@ -0,0 +1,9 @@
+word = 'assume'
+s = 'audacity'
+
+print word[4:6]
+print word[4:]
+print word [:4]
+print word[:]
+
+print 'U' + s[2:]
