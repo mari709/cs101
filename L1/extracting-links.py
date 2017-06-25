f7e79903e4870eecf8d7c91a2cd0a2681c0a021a
Marina M <mansumdp@gmail.com>
2017-06-25 17:52:01 -0300

    Add extracting-links

---
+++ L1/extracting-links.py
@@ -0,0 +1,13 @@
+page = '<ul class="nav-bar"> <li><a href="https:example.com/about">about</a></li> </ul>'
+
+start_link = page.find('<a href=')
+print start_link #position start link
+
+start_quote = page.find('"', start_link)
+print start_quote #position start first quote of link
+
+end_quote = page.find('"', start_quote + 1)
+print end_quote #position end quote
+
+url = page[start_quote + 1: end_quote]
+print url
